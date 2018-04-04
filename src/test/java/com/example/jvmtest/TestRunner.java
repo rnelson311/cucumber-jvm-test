6d07d9dbea6430ddf9430714ecee65dea718ca12
Robert Nelson <nelson.robert.edward@gmail.com>
2018-04-04 10:15:52 -0500

    Simple attempt at using the cucumber-jvm

---
+++ src/test/java/com/example/jvmtest/TestRunner.java
@@ -0,0 +1,15 @@
+package com.example.jvmtest;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.*;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"pretty",
+    "html:target/test-report",
+    "json:target/test-report.json",
+    "junit:target/test-report.xml"},
+    features="src/test/specs")
+
+public class TestRunner {
+}
